25243ed069e3bb1bf2ee27d65887f6d56724bbf7
anniebae <bae.annie@gmail.com>
2014-11-05 22:03:38 -0500

    w07d03 last commit of the night

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -19,10 +19,11 @@ function createDivForLetters(word){
 
 	for (var i = 0; i < word.length; i++) {
 		var div = document.createElement("div");
-		div.style.width = "25px";
-		div.style.height = "25px";
+		div.style.width = "50px";
+		div.style.height = "50px";
 		div.style.background = "white";
 		div.style.border = "solid 1px gray";
+		div.style.display = "inline-block";
 		div.innerHTML = word[i];
 		$('#random-word').append(div);
 	};
