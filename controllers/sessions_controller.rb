0ed0b95de5416146a030134120d1e4a415ceb6d3
anniebae <bae.annie@gmail.com>
2015-01-06 15:36:41 -0500

    remove center tags

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -5,13 +5,6 @@ class SessionsController < ApplicationController
 
   post '/' do
     user = User.find_by({username: params[:username]})
-    # if user.password == params[:password]
-    #   session[:current_user] = user.id
-    #   redirect '/profiles'
-    # else
-    #   redirect '/users/new'
-    # end
-
     if user
       if user.password == params[:password]
         session[:current_user] = user.id
@@ -22,14 +15,13 @@ class SessionsController < ApplicationController
       end
     else
       redirect '/users/new'
-    end     
+    end
   end
 
   get '/error' do
   @error = "** wrong password **"
   erb :'sessions/new'
   end
-
 
   delete '/' do
     session[:current_user] = nil
